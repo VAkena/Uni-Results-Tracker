84afc99d00a2447f4b99aa93b449e5c49e0adc99
vakena <vakena@outlook.com>
2020-03-07 17:11:33 +1100

    maintenance

--- autumn2020.py
+++ autumn2020.py
@@ -1,31 +1,30 @@
 # UTS grade distributions
-grade_distributions = {"Fail": range(0,50), "Pass": range(50,64), "Credit": range(65,74), "Distinction": range(75, 84), "High Distinction": range(85,100)}
+grade_distributions = {"Fail": range(0,49), "Pass": range(50,64), "Credit": range(65,74), "Distinction": range(75, 84), "High Distinction": range(85,100)}
 
 autumn_courses_2020 = ("31272 Project Management and the Professional", "41025 Introduction to Software Development", "41182 System Security", "31277 Routing and Switching Essentials")
 
 assessment_count = {autumn_courses_2020[1]: 3, autumn_courses_2020[2]: 3, autumn_courses_2020[3]: 3}
 # Assessment weighting
-pmp_assessment_weighting = {"Assessment 1": 20, "Assessment 2": 30, "Assessment 3": 10, "Assessment 4": 40}
-isd_assessment_weighting = {"Assessment 1": 30, "Assessment 2": 50, "Assessment 3": 20}
-rse_assessment_weighting = {"Assessment 1": 40, "Assessment 2": 45, "Assessment 3": 15}
-ss_assessment_weighting = {"Assessment 1": 40, "Assessment 2": 40, "Assessment 3": 20}
+pmp_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.2), "Assessment 2": "{:.0%}".format(0.3), "Assessment 3": "{:.0%}".format(0.1), "Assessment 4": "{:.0%}".format(0.4)}
+isd_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.3), "Assessment 2": "{:.0%}".format(0.5), "Assessment 3": "{:.0%}".format(0.2)}
+rse_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.4), "Assessment 2": "{:.0%}".format(0.45), "Assessment 3": "{:.0%}".format(0.15)}
+ss_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.4), "Assessment 2": "{:.0%}".format(0.4), "Assessment 3": "{:.0%}".format(0.2)}
 
-print("Assessment Count: \n")
+# Output Formatting
+# PMP is separated because it has four assessments
+print("Autumn 2020 Assessment Overview:\n")
 print(autumn_courses_2020[0] + ": 4")
-print(pmp_assessment_weighting[0:])
+for a in pmp_assessment_weighting:
+    print(a, pmp_assessment_weighting[a])
+print("Current Completion Percentage:")
+print("Current Grade: \n")
 
+# Output loop for the other courses
 for subject in assessment_count:
     print(subject + ": 3") 
-
-
+    for b in isd_assessment_weighting:
+        print(b, isd_assessment_weighting[b])
 
 # Final grade calculation
 def calculate_grade():
     pass
-
-# Output results
-# for subject in autumn_courses_2020:
-#     print(subject[0:] + ":") 
-#     print("Current Percentage: \n")
-#     print("Total Course Percentage: \n")
-#     print("Current Grade: \n")
