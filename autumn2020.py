0da1d5d5ad005b7b3c20d0e9535f2b1d41ca28b3
vakena <vakena@outlook.com>
2020-03-07 13:48:50 +1100

    script for autumn 2020 added

---
+++ autumn2020.py
@@ -0,0 +1,6 @@
+autumn_courses_2020 = ("31272 Project Management and the Professional", "41025 Introduction to Software Development", "41182 System Security", "31277 Routing and Switching Essentials")
+# Create dictionary of weighting
+
+# Perform calculations
+# Output results
+print(autumn_courses_2020)
