13be4c088132b7affe113675839503f2bde9d75b
vakena <vakena@outlook.com>
2020-03-07 19:57:35 +1100

    started working on the menu system

--- autumn2020.py
+++ autumn2020.py
@@ -11,28 +11,51 @@ isd_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.3), "Assessment 2"
 rse_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.4), "Assessment 2": "{:.0%}".format(0.45), "Assessment 3": "{:.0%}".format(0.15)}
 ss_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.4), "Assessment 2": "{:.0%}".format(0.4), "Assessment 3": "{:.0%}".format(0.2)}
 
-# Default information view
-print("Autumn 2020 Assessment Overview:\n")
-print(autumn_courses_2020[0] + ": 4")
-for a in pmp_assessment_weighting:
-    print(a, pmp_assessment_weighting[a])
-print("Current Percentage:")
-print("Current Grade: \n")
+def view_progess():
+    # Default information view
+    print("Autumn 2020 Assessment Overview:\n")
+    print(autumn_courses_2020[0] + ": 4")
+    for a in pmp_assessment_weighting:
+        print(a, pmp_assessment_weighting[a])
+    print("Current Percentage:")
+    print("Current Grade: \n")
 
-print(autumn_courses_2020[1] + ": 3")
-for b in isd_assessment_weighting:
-    print(b, isd_assessment_weighting[b])
-print("Current Percentage:")
-print("Current Grade: \n")
+    print(autumn_courses_2020[1] + ": 3")
+    for b in isd_assessment_weighting:
+        print(b, isd_assessment_weighting[b])
+    print("Current Percentage:")
+    print("Current Grade: \n")
 
-print(autumn_courses_2020[2] + ": 3")
-for c in ss_assessment_weighting:
-    print(c, ss_assessment_weighting[c])
-print("Current Percentage:")
-print("Current Grade: \n")
+    print(autumn_courses_2020[2] + ": 3")
+    for c in ss_assessment_weighting:
+        print(c, ss_assessment_weighting[c])
+    print("Current Percentage:")
+    print("Current Grade: \n")
 
-print(autumn_courses_2020[3] + ": 3")
-for d in rse_assessment_weighting:
-    print(d, rse_assessment_weighting[d])
-print("Current Percentage:")
-print("Current Grade: \n")
+    print(autumn_courses_2020[3] + ": 3")
+    for d in rse_assessment_weighting:
+        print(d, rse_assessment_weighting[d])
+    print("Current Percentage:")
+    print("Current Grade: \n")
+    
+def update_results():
+    pass
+    
+def help_me():
+    pass
+
+def menu_options(i):
+    switcher={
+            "v": view_progess(),
+            "u":update_results(),
+            "h":help_me(),
+            }
+    return switcher.get(i,"Please try again!")
+user_choice = input("What do you want to do? Choice (v/u/x/h): ")
+
+# user_choice = input("What do you want to do? Choice (v/u/x/h): ")
+# while user_choice != 'x':
+#     if user_choice == 'v':
+#         view_progess()
+#     else:
+#         user_choice()
