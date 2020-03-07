465ed7fb838c31a24907731eb1de09b036b6cf4f
vakena <vakena@outlook.com>
2020-03-07 15:18:50 +1100

    assessment count implemented

--- autumn2020.py
+++ autumn2020.py
@@ -1,6 +1,21 @@
-autumn_courses_2020 = ("31272 Project Management and the Professional", "41025 Introduction to Software Development", "41182 System Security", "31277 Routing and Switching Essentials")
-# Create dictionary of weighting
+# UTS grade distributions
+grade_distributions = {"Fail": range(0,50), "Pass": range(50,64), "Credit": range(65,74), "Distinction": range(75, 84), "High Distinction": range(85,100)}
 
-# Perform calculations
+autumn_courses_2020 = ("31272 Project Management and the Professional", "41025 Introduction to Software Development", "41182 System Security", "31277 Routing and Switching Essentials")
+
+assessment_count = {autumn_courses_2020[1]: 3, autumn_courses_2020[2]: 3, autumn_courses_2020[3]: 3}
+print("Assessment Count: \n")
+print(autumn_courses_2020[0] + ": 4")
+for subject in assessment_count:
+    print(subject + ": 3") 
+
+# Final grade calculation
+def calculate_grade():
+    pass
+
 # Output results
-print(autumn_courses_2020)
+# for subject in autumn_courses_2020:
+#     print(subject[0:] + ":") 
+#     print("Current Percentage: \n")
+#     print("Total Course Percentage: \n")
+#     print("Current Grade: \n")
