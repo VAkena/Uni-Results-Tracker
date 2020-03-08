fe05663d4c2b4aa93baf856335f65552602d5d51
Vinecnt Akena <vakena@outlook.com>
2020-03-08 18:27:56 +1100

    basic results calculations

--- autumn2020.py
+++ autumn2020.py
@@ -17,29 +17,40 @@ ss_assessment_weighting = {"Assessment 1": "{:.0%}".format(0.4), "Assessment 2":
 print("Autumn 2020 Assessment Overview:\n")
 
 # Project Management and the Professional
+# Display current percentage
+default_percentage = "{:.0%}".format(0.0)
+a1_max = 20
+a1_weight = 20
+a2_max = 30
+a3_max = 10
+a4_max = 40
+a1_score = input("What did you score on Assessment 1? ")
+a1_final = ((float(a1_score) / float(a1_max) * float(a1_weight)))
+
 print(autumn_courses_2020[0] + ": 4")
 for a in pmp_assessment_weighting:
     print(a, pmp_assessment_weighting[a])
-print("Current Percentage:")
+print("Current Percentage: " + str(a1_final))
 print("Current Grade: \n")
+
 
 # Introduction to Software Development
 print(autumn_courses_2020[1] + ": 3")
 for b in isd_assessment_weighting:
     print(b, isd_assessment_weighting[b])
-print("Current Percentage:")
+print("Current Percentage: " + default_percentage)
 print("Current Grade: \n")
 
 # System Security
 print(autumn_courses_2020[2] + ": 3")
 for c in ss_assessment_weighting:
     print(c, ss_assessment_weighting[c])
-print("Current Percentage:")
+print("Current Percentage: "  + default_percentage)
 print("Current Grade: \n")
 
 # Routing and Switching Essentials
 print(autumn_courses_2020[3] + ": 3")
 for d in rse_assessment_weighting:
     print(d, rse_assessment_weighting[d])
-print("Current Percentage:")
+print("Current Percentage: "  + default_percentage)
 print("Current Grade: \n")
